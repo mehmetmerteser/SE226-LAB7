9382395793e42bd447df6749735a75bafeb7b1d4
mehmetmerteser <153126285+mehmetmerteser@users.noreply.github.com>
2025-04-10 14:52:17 +0300

    Create stats.py

---
+++ string_package/stats.py
@@ -0,0 +1,14 @@
+
+
+def count_characters(s):
+    return len(s)
+
+def count_words(s):
+    return len(s.split())
+
+def average_word_length(s):
+    words = s.split()
+    return sum(len(word) for word in words) / len(words) if words else 0
+
+if __name__ == "__main__":
+    print("Avg word length:", average_word_length("hello world!"))
